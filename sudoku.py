2249696f1557fd1295b9a5aa3dad54a44d4c2f71
5inu <nijhuis.karelarjan@gmail.com>
2021-11-24 21:15:13 +0100

    Assignment done

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,9 @@
+# Karel Nijhuis
+# 12904392
+# Solves a sudoku puzzle
+
 from __future__ import annotations
-from typing import Iterable, Sequence
+from typing import Iterable
 
 
 class Sudoku:
@@ -37,15 +41,7 @@ class Sudoku:
 
     def value_at(self, x: int, y: int) -> int:
         """Returns the value at x,y."""
-        value = -1
-
-        for i in range(9):
-            for j in range(9):
-                if i == x and j == y:
-                    row = self._grid[y]
-                    value = int(row[x])
-
-        return value
+        return int(self._grid[y][x])
 
     def options_at(self, x: int, y: int) -> Iterable[int]:
         """Returns all possible values (options) at x,y."""
@@ -80,28 +76,18 @@ class Sudoku:
 
         for y in range(9):
             for x in range(9):
-                if self.value_at(x, y) == 0 and next_x == -1 and next_y == -1:
+                if int(self._grid[y][x]) == 0 and next_x == -1 and next_y == -1:
                     next_x, next_y = x, y
 
         return next_x, next_y
 
     def row_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th row."""
-        values = []
-
-        for j in range(9):
-            values.append(self.value_at(j, i))
-
-        return values
+        return [int(self._grid[i][j]) for j in range(9)]
 
     def column_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th column."""
-        values = []
-
-        for j in range(9):
-            values.append(self.value_at(i, j))
-
-        return values
+        return [int(self._grid[j][i]) for j in range(9)]
 
     def block_values(self, i: int) -> Iterable[int]:
         """
@@ -111,16 +97,11 @@ class Sudoku:
         3 4 5
         6 7 8
         """
-        values = []
 
         x_start = (i % 3) * 3
         y_start = (i // 3) * 3
 
-        for x in range(x_start, x_start + 3):
-            for y in range(y_start, y_start + 3):
-                values.append(self.value_at(x, y))
-
-        return values
+        return [int(self._grid[y][x]) for x in range(x_start, x_start + 3) for y in range(y_start, y_start + 3)]
 
     def is_solved(self) -> bool:
         """
@@ -129,20 +110,15 @@ class Sudoku:
         """
         values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        result = True
-
         for i in range(9):
-            for value in values:
-                if value not in self.column_values(i):
-                    result = False
+            if sorted(self.block_values(i)) != values:
+                return False
+            if sorted(self.column_values(i)) != values:
+                return False
+            if sorted(self.row_values(i)) != values:
+                return False
 
-                if value not in self.row_values(i):
-                    result = False
-
-                if value not in self.block_values(i):
-                    result = False
-
-        return result
+        return True
 
     def __str__(self) -> str:
         representation = ""
